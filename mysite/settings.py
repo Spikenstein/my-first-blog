ba2773736b54d8898b36a3aef791fd1d99e7fb8c
Spikenstein <stephane.picq@gmail.com>
2016-06-24 17:34:25 +0200

    dgp

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'blog',
+    'dgp',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'Europe/Paris'
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 import django.contrib.auth.views
 
-from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
@@ -10,4 +9,5 @@ urlpatterns = [
     url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
     url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
 	url(r'', include('blog.urls')),
+    url(r'^dgp/', include('dgp.urls')),
 ]
